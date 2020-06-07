f94d3cf80598b93f8e58fef92e2fbbb746857395
Benedict Xavier <benedict.xavier.e@gmail.com>
2020-06-07 14:42:23 +0530

    prepared for deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 const users = require('./routers/api/users');
@@ -13,13 +13,22 @@ connectDB();
 //Init middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API running'));
-
 //define routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 app.use('/api/auth', auth);
+
+/// Serve Static Assets in production
+
+if (process.env.NODE_ENV === 'production') {
+	//Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 
