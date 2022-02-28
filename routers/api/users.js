7dd289fcc05e7d331b68d267668ddc1c02c7e469
Benedict <benedict.xavier.e@gmail.com>
2022-02-28 23:21:05 +0530

    Update users.js

--- routers/api/users.js
+++ routers/api/users.js
@@ -52,7 +52,7 @@ router.post(
 				}
 			};
 
-			jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 36000 }, (err, token) => {
+			jwt.sign(payload, process.env.jwtSecret, { expiresIn: 36000 }, (err, token) => {
 				if (err) {
 					throw err;
 				}
